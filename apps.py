7d6f99d4a1d10079cca499a44753c9482aabcce9
andrey <a.lyutyy@big3.ru>
2024-07-22 15:33:31 +0300

    Revert "init project"
    This reverts commit 78d73984398059e164fc6376f04827e2120a5060.

--- apps.py
+++
@@ -1,9 +0,0 @@
-from functools import lru_cache
-
-from django.apps import AppConfig
-
-
-class OvgaConfig(AppConfig):
-    name = 'integrations.ovga_integration'
-    verbose_name = "Интеграция с ОВГА"
-    additional = True
--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from integrations.ovga_integration.serializers import TransportPassRequestUpdateView
-
-urlpatterns = [
-    path('transport-pass-request/', TransportPassRequestUpdateView.as_view(), name='trans_pass_update'),
-]
